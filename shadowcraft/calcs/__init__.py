ddc6644c7fbd174a2e9144177063b9c455f50106
Pathal <pathal.ej@gmail.com>
2014-12-08 13:13:09 -0500

    6.0 ICD change
    - pvp trinket

--- shadowcraft/calcs/__init__.py
+++ shadowcraft/calcs/__init__.py
@@ -31,7 +31,7 @@ class DamageCalculator(object):
 
     def __init__(self, stats, talents, glyphs, buffs, race, settings=None, level=100, target_level=None, char_class='rogue'):
         self.WOW_BUILD_TARGET = '6.0.3' # should reflect the game patch being targetted
-        self.SHADOWCRAFT_BUILD = '1.05' # <1 for beta builds, 1.00 is GM, >1 for any bug fixes, reset for each warcraft patch
+        self.SHADOWCRAFT_BUILD = '1.051' # <1 for beta builds, 1.00 is GM, >1 for any bug fixes, reset for each warcraft patch
         self.tools = class_data.Util()
         self.stats = stats
         self.talents = talents
