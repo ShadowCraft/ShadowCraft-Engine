89f8aca86770b24f6a15fef46099b063d1b9f193
Fierydemise <fierydemise@gmail.com>
2016-11-30 11:46:47 -0500

    Modifiers Whitelist by Default, also Documentation

--- shadowcraft/objects/modifiers.py
+++ shadowcraft/objects/modifiers.py
@@ -1,5 +1,10 @@
 from shadowcraft.core import exceptions
 
+#ModifierList contains all modifiers needed for dps computation.
+#ModifierList is used to compile all modifiers into a single lumped modifier per damage source.
+#Typical use case registers all modifiers needed at the beginning of the computation with a
+#None value and then updates later.
+#Before final damage computation a dict is compiled and used for damage computation
 class ModifierList(object):
 	def __init__(self, sources):
 		self.sources = sources
@@ -14,25 +19,26 @@ class ModifierList(object):
 	def update_modifier_value(self, modifier_name, value):
 		self.modifiers[modifier_name].value = value
 
-	def compile__modifier_dict(self):
+	def compile_modifier_dict(self):
 		lumped_modifier = {s:1 for s in self.sources}
 		for mod in self.modifiers.values():
-			if mod.whitelist:
+			if not mod.blacklist:
 				for ability in mod.ability_list:
 					lumped_modifier[ability] *= mod.value
-			if mod.blacklist:
+			else:
 				for ability in self.sources:
 					if not ability in mod.ability_list:
 						lumped_modifier[ability] *= mod.value
 		return lumped_modifier
 
+#DamageModifier specifies any type of modifier applied to ability damage.
+#Each modifier is specified as a value applied to either a whitelist or blacklist of abilities.
+#Whitelist is default since it is more compact for most modifiers
+#but all damage modifiers can be represented either way
 class DamageModifier(object):
-	def __init__(self, name, value, ability_list, whitelist=True, blacklist=False):
-		if not (whitelist ^ blacklist):
-			raise exceptions.InvalidInputException(_('Damage Modifier {mod} must be either a blacklist or whitelist').format(mod=name))
+	def __init__(self, name, value, ability_list, blacklist=False):
 		self.name = name
 		self.value = value
 		self.ability_list = ability_list
 		self.blacklist = blacklist
-		self.whitelist = whitelist
 
