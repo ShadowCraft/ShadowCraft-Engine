81b87dc207086d84e400e1937fbea5b0f5c7c417
Tim Wojtulewicz <timwoj@ieee.org>
2017-03-19 19:59:33 -0700

    Restore setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+setup(
+    name='ShadowCraft-Engine',
+    url='http://github.com/ShadowCraft/ShadowCraft-Engine/',
+    version='7.1.5',
+    packages=[
+        'shadowcraft',
+        'shadowcraft.calcs',
+        'shadowcraft.calcs.rogue',
+        'shadowcraft.calcs.rogue.Aldriana',
+        'shadowcraft.core',
+        'shadowcraft.objects'
+    ],
+    license='LGPL',
+    long_description=open('README').read(),
+)
